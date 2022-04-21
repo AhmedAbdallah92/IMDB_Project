75af0f0b4fa23cb69b2a9dfbbc43f45e8542b43e
AhmedAbdallah92 <a.abdallah.aa91@gmail.com>
2022-04-21 04:53:43 +0200

    edit

--- IMDB_project/Models/Movie.cs
+++ IMDB_project/Models/Movie.cs
@@ -11,7 +11,7 @@ namespace IMDB_project.Models
         public int id { get; set; }
         public string name { get; set; }
         public string desc { get; set; }
-        public int genre { get; set; }
+        public string genre { get; set; }
         public string imge { get; set; }
     }
 }
